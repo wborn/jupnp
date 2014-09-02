b81b7175ebd4710ac3b2b074a63d66c0c10860f8
Kai Kreuzer <kai@openhab.org>
2014-09-02 13:32:21 +0200

    updated license headers
    Signed-off-by: Kai Kreuzer <kai@openhab.org> (github: @kaikreuzer)

--- tests/core/src/test/java/example/localservice/SwitchPowerModerated.java
+++ tests/core/src/test/java/example/localservice/SwitchPowerModerated.java
@@ -1,8 +1,7 @@
-/*
- * Copyright (C) 2013 4th Line GmbH, Switzerland
+/**
+ * Copyright (C) 2014 4th Line GmbH, Switzerland and others
  *
- * The contents of this file are subject to the terms of either the GNU
- * Lesser General Public License Version 2 or later ("LGPL") or the
+ * The contents of this file are subject to the terms of either the
  * Common Development and Distribution License Version 1 or later
  * ("CDDL") (collectively, the "License"). You may not use this file
  * except in compliance with the License. See LICENSE.txt for more
@@ -12,6 +11,7 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  */
+
 package example.localservice;
 
 import org.jupnp.binding.annotations.UpnpStateVariable;
