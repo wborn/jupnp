29fe0e5e174aa9d4323717ca62b0ac9ddac378e8
Wouter Born <github@maindrain.net>
2025-01-01 15:22:10 +0100

    Update license headers to 2025 (#266)
    Signed-off-by: Wouter Born <github@maindrain.net>

--- bundles/org.jupnp/src/test/java/org/jupnp/data/SampleDeviceEmbeddedTwo.java
+++ bundles/org.jupnp/src/test/java/org/jupnp/data/SampleDeviceEmbeddedTwo.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011-2024 4th Line GmbH, Switzerland and others
+ * Copyright (C) 2011-2025 4th Line GmbH, Switzerland and others
  *
  * The contents of this file are subject to the terms of the
  * Common Development and Distribution License Version 1 or later
